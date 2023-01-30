7c67d3e0cf0c92cc87144b479b461ba318d12581
FreshWaterGames <70551495+FreshWaterGames@users.noreply.github.com>
2023-01-30 07:48:18 -0500

    walker added
    pls dont ruin project

--- WinterGameJam/Scripts/Walker.gd
+++ WinterGameJam/Scripts/Walker.gd
@@ -1,20 +1,22 @@
 extends Enemy
 
+export var Speed = 200
+
 var Direction = Vector2.LEFT
 var Velocity = Vector2.ZERO
+var Gravity = 700
 
 onready var Raycast = $RayCast2D
 
 func _ready():
 	#set_physics_process(false)
-	pass
+	Velocity.x = Speed
 
 func _physics_process(delta: float) -> void:
-	#Velocity.y += Gravity * delta
+	Velocity.y += Gravity * delta
 	#if touch wall/raycast dosent touch floor mulitply by -1 to change direction
 	if is_on_wall() || !Raycast.is_colliding():
-		Direction *= -1
+		Velocity.x *= -1
 		Raycast.position.x *= -1
-	Velocity = Direction * 2500
-	move_and_slide(Velocity, Vector2.UP)
+	Velocity.y = move_and_slide(Velocity, Vector2.UP).y
 
