1048fb1e4d2b937be84fbbcc55ea0be723084ca7
dodo3142 <85135076+dodo3142@users.noreply.github.com>
2023-02-02 17:58:10 +0200

    remove it

--- WinterGameJam/Scripts/BasicEnemy.gd
+++ WinterGameJam/Scripts/BasicEnemy.gd
@@ -5,11 +5,9 @@ export var MaxHealth = 100
 export var Attack = 30
 export var FrameFreezeTime = 0.2
 onready var Health = MaxHealth
-onready var HealthBar = get_node("HealthBar")
-
 
 func _ready():
-	HealthBar._on_max_health_updated(MaxHealth)
+	pass
 
 func _process(_delta):
 	if Health <= 0:
@@ -19,7 +17,6 @@ func _process(_delta):
 func TakeDamage(Damage):
 	Health -= Damage
 	FrameFreeze(FrameFreezeTime)
-	HealthBar._on_health_updated(Health, Health)
 
 func FrameFreeze(duration):
 	set_process(false)
